90bc832432ed76f6b73543d971f720e312d47ca7
Jose Saldana <jsaldana@unizar.es>
2016-04-19 09:57:04 +0200

    Added some comments to avoid confusion

--- scripts_start_ap_odin/start_odin.sh
+++ scripts_start_ap_odin/start_odin.sh
@@ -7,11 +7,13 @@ ifconfig mon0 up
 ifconfig wlan0 up
 
 # add this route in order to permit control from Unizar networks                
-# traffic from these networks will not go through the default gateway          
+# traffic from these networks will not go through the default gateway
+# NOTE: The next two lines are just for our Unizar setup, so you can remove them
 route add -net 155.210.158.0 netmask 255.255.255.0 gw 155.210.157.254 eth0      
 route add -net 155.210.156.0 netmask 255.255.255.0 gw 155.210.157.254 eth0      
 
-# set the default gateway where masquerading is being performed                 
+# set the default gateway where masquerading is being performed
+# NOTE: This may vary according to your setup. Add your default gateway as needed
 route del default gw 155.210.157.254
 route add default gw 192.168.1.129
 
