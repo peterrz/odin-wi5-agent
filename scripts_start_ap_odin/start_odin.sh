30e38dce7f09d9f7f1eb4c5ff8c503b8516e8822
Jose Saldana <jsaldana@unizar.es>
2016-04-19 14:58:52 +0200

    More comments added

--- scripts_start_ap_odin/start_odin.sh
+++ scripts_start_ap_odin/start_odin.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 echo "interfaces"
+#put wlan0 down before creating mon0
 ifconfig wlan0 down
+#add the monitor device
 iw phy phy0 interface add mon0 type monitor
+#you should not define the channel here, so we have commented the next line
 #iw dev wlan0 set channel 1
+#initiate mon0
 ifconfig mon0 up
+#initiate wlan0. You must have it active, otherwise things do not work
 ifconfig wlan0 up
 
 # add this route in order to permit control from Unizar networks                
@@ -17,12 +22,12 @@ route add -net 155.210.156.0 netmask 255.255.255.0 gw 155.210.157.254 eth0
 route del default gw 155.210.157.254
 route add default gw 192.168.1.129
 
-# if you have put click and start-up scripts in a usb device
+# if you have put click and start-up scripts in a usb device, mount the USB
 mkdir -p /mnt/usb
 mount /dev/sda1 /mnt/usb/
 cd /mnt/usb/
 
-# start ovs and click
+# start ovs and click with the corresponding scripts
 ./script_start_ovs.sh
 sleep 2
 ./script_start_click.sh
