641dbcce34827df03590c42c0ddf677a24f27814
Luis Sequeira <sequeira@unizar.es>
2016-04-29 10:00:04 +0200

    For testing openflow rules
    For testing openflow rules

--- scripts_start_ap_odin/script_start_click.sh
+++ scripts_start_ap_odin/script_start_click.sh
@@ -4,5 +4,6 @@ ifconfig ap up
 ovs-vsctl --db=tcp:192.168.1.5:6632 add-port br0 ap
 sleep 3
 ovs-ofctl add-flow br0 in_port=1,dl_type=0x0800,nw_src=192.168.1.129,nw_dst=192.168.1.5,nw_proto=6,tp_dst=6777,actions=output:LOCAL
-
-
+# For testing
+# ovs-ofctl add-flow br0 in_port=3,dl_type=0x0800,nw_proto=17,tp_dst=67,actions=output:1,2
+# ovs-ofctl add-flow br0 in_port=2,dl_type=0x0800,nw_proto=17,tp_dst=68,actions=output:1,3
