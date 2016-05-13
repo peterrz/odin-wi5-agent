c0a4cccbb433e007fce23de09ca5688b1e422331
luissequeira <sequeira@unizar.es>
2016-05-13 17:06:12 +0200

    Fix some bugs in handler_channel

--- src/odinagent.cc
+++ src/odinagent.cc
@@ -1888,7 +1888,7 @@ OdinAgent::write_handler(const String &str, Element *e, void *user_data, ErrorHa
 			if (agent->_debug_level % 10 > 0)
 				fprintf(stderr, "[Odinagent.cc] ########### Changing AP to channel %i\n", channel);
       std::stringstream ss;
-      ss << "hostapd_cli chan_switch " << _count_csa_beacon_default << " " << channel;
+      ss << "hostapd_cli chan_switch " << agent->_count_csa_beacon_default << " " << channel;
       std::string str = ss.str();
       char *cstr = new char[str.length() + 1];
       strcpy(cstr, str.c_str());
